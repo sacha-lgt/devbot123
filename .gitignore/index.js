7386c7faf2e0a872cd069c166dc191102cc28c6d
skqh <36108894+skqh@users.noreply.github.com>
2018-02-03 18:41:03 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,13 +12,13 @@ bot.login("NDA1MDQ1OTY2OTU4NDkzNzI2.DVdrDQ.ywRFq2JcwR80PRdhrv4Dh7l5qi4");
 
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
-		message.channel.sendMessage("Liste des commandes:\n- *helpfr = Avoir les commandes d'aide en francais\n- *helpes = Have help commands in english");
+		message.channel.sendMessage("Commands:\n- *helpfr = Avoir les commandes d'aide en francais\n- *helpes = Have help commands in english");
 	}
 	if (message.content === prefix + "helpfr"){
-		message.channel.sendMessage("---COMMANDES BOT FR---\n- *web = Lien de redirection vers le site officiel de DevPub !\n- *CONFIRM = Confime toi afin d'obtenir le grade Membre(En cours de dev...)\n- *");
+		message.author.sendMessage("---COMMANDES BOT FR---\n- *web = Lien de redirection vers le site officiel de DevPub !\n- *CONFIRM = Confime toi afin d'obtenir le grade Membre(En cours de dev...)\n- *");
 	}
 	if (message.content === prefix + "helpes"){
-		message.channel.sendMessage("Commandes Bot Anglaise A Definir !");
+		message.author.sendMessage("Commandes Bot Anglaise A Definir !");
 	}
 	if (message.content === 'ping') {
    		message.reply('pong !')
@@ -26,7 +26,9 @@ bot.on('message', message => {
 	if (message.content === prefix + "site"){
 		message.channel.sendMessage("Commandes Bot Anglaise A Definir !");
 	}
-	
+	if (message.content === prefix + "web"){
+		message.author.sendMessage("WebSite of DevPub: ....");
+	}		
 });
 bot.on('guildMemberAdd', function (member) {
     member.createDM().then(function (channel) {
