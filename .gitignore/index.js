2331ac72d4a9615f8dc749fda0b4614b54df72fc
skqh <36108894+skqh@users.noreply.github.com>
2018-02-03 17:37:16 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,8 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-	bot.user.setGame("Commandes: *help");
-	console.log("Connected");
+	bot.user.setGame("Commands: *help");
+	console.log("Bot Connected On The Server !");
 });
 
 bot.login("NDA1MDQ1OTY2OTU4NDkzNzI2.DVdrDQ.ywRFq2JcwR80PRdhrv4Dh7l5qi4");
@@ -13,6 +13,12 @@ bot.login("NDA1MDQ1OTY2OTU4NDkzNzI2.DVdrDQ.ywRFq2JcwR80PRdhrv4Dh7l5qi4");
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
 		message.channel.sendMessage("Liste des commandes:\n- *helpfr = Avoir les commandes d'aide en francais\n- *helpes = Have help commands in english");
+	}
+	if (message.content === prefix + "helpfr"){
+		message.channel.sendMessage("Commandes Bot Francaise A Definir !");
+	}
+	if (message.content === prefix + "helpes"){
+		message.channel.sendMessage("Commandes Bot Anglaise A Definir !");
 	}
 	if (message.contact === "Salut"){
 		message.reply("Bien le bonjour !");
