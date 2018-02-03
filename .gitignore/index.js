3e5aa5f703c3b7c51d5e3a78920867456de450b2
skqh <36108894+skqh@users.noreply.github.com>
2018-02-03 17:14:36 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,21 @@
+const  Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+	bot.user.setGame("Commandes: *help");
+	console.log("Connected");
+});
+
+bot.login("NDA1MDQ1OTY2OTU4NDkzNzI2.DVdrDQ.ywRFq2JcwR80PRdhrv4Dh7l5qi4");
+
+bot.on('message', message => {
+	if (message.content === prefix + "help"){
+		message.channel.sendMessage("Liste des commandes:/n -*help");
+	}
+	if (message.contact === "Salut"){
+		message.reply("Bien le bonjour !");
+		console.log("Commande Salut effectué");
+	}
+});
