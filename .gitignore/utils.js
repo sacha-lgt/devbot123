740dcd77e5faa64f2fafe1be44b145aa78d74d9f
skqh <36108894+skqh@users.noreply.github.com>
2018-03-08 15:01:58 +0100

    Update utils.js

--- .gitignore/utils.js
+++ .gitignore/utils.js
@@ -1,11 +1,16 @@
-exports.formatTime = (time) =>
-{
-    let days = Math.floor(time / 86400)
-    time %= 86400
-    let hours = Math.floor(time / 3600)
-    time %= 3600
-    let minutes = Math.floor(time / 60)
-    let seconds = Math.floor(time % 60)
+const fs = require('fs')
 
-    return days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes et ' + seconds + ' secondes'
-}
+exports.initBot = (index) =>
+{
+    exports.formatTime = (time) =>
+    {
+       let days = Math.floor(time / 86400)
+        time %= 86400
+        let hours = Math.floor(time / 3600)
+        time %= 3600
+        let minutes = Math.floor(time / 60)
+        let seconds = Math.floor(time % 60)
+
+        return days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes et ' + seconds + ' secondes'
+    }
+}   
