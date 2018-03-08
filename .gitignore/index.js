26a5cbd8d3bbd87f244da31f2c31c34a5e6d4d98
skqh <36108894+skqh@users.noreply.github.com>
2018-03-08 15:12:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -28,15 +28,12 @@ bot.on('message', message => {
   	if(message.content === prefix + "botinfos"){
     	let bicon = bot.user.displayAvatarURL;
     	let botembed = new Discord.RichEmbed()
-    	.setDescription("Bot Information"
+    	.setDescription("Bot Information")
     	.setColor("#15f153")
     	.setThumbnail(bicon)
     	.addField("Bot Name", bot.user.username)
-    	.addField("Created On", bot.user.createdAt);			
-	)
-	.setFooter("By skqh_");
+    	.addField("Created On", bot.user.createdAt)			
 	
-
     	return message.channel.send(botembed);
   	}
 	if (message.content === prefix + "help"){
